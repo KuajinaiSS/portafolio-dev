883d1d635e46cad413e1fa920da0360606282a45
KuajinaiSS <kuajinaiss@gmail.com>
2025-11-28 01:34:56 -0300

    fix: final commit to fix pagar name to Pagar

--- src/assets/images/vueltaAlMundo365Platos/index.ts
+++ src/assets/images/vueltaAlMundo365Platos/index.ts
@@ -2,5 +2,5 @@ export { default as Categorias } from "./Categorias.jpg";
 export { default as Comprar } from "./Comprar.jpg";
 export { default as Editar } from "./Editar.jpg";
 export { default as Login } from "./Login.jpg";
-export { default as Pagar } from "./pagar.jpg";
+export { default as Pagar } from "./Pagar.jpg";
 export { default as Productos } from "./Productos.jpg";
