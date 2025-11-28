da2ebc8830db2f76849a47f8b58af65cab2e9393
KuajinaiSS <kuajinaiss@gmail.com>
2025-11-28 00:42:51 -0300

    feature: update images for projects

---
+++ src/assets/images/vueltaAlMundo365Platos/index.ts
@@ -0,0 +1,6 @@
+export { default as Categorias } from "./Categorias.jpg";
+export { default as Comprar } from "./Comprar.jpg";
+export { default as Editar } from "./Editar.jpg";
+export { default as Login } from "./Login.jpg";
+export { default as Pagar } from "./Pagar.jpg";
+export { default as Productos } from "./Productos.jpg";
